ac4df1284fb87d43ef91d4324583b7faef08e675
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-11-19 10:44:00 -0500

    small param fixes

--- app/admin/testimonials.rb
+++ app/admin/testimonials.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Testimonial do
 
-  permit_params :title, :description, :developer_id, :is_live
+  permit_params :title, :description, :works_with_id, :is_live
 
   form(:html => {:multipart => true}) do |f|
     f.inputs do
       f.input :title
       f.input :description, :input_html => {:class => 'tinymce'}, :label => 'Testimonial Copy'
-      f.input :developer_id, :label => 'Which "Works With" does this support?', :as => :select, :collection => WorksWith.all.map{|w| ["#{w.title}", w.id]}
+      f.input :works_with_id, :label => 'Which "Works With" does this support?', :as => :select, :collection => WorksWith.all.map{|w| ["#{w.title}", w.id]}
       f.input :is_live, :label => 'Publish?', :as => :radio
     end
     f.actions
