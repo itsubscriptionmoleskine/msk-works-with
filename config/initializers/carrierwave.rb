377b1abc5a5c8932ef7d75534b1d6df1cf6de180
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-12-03 09:59:26 -0500

    gcloud integration, admin changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,14 @@
 CarrierWave.configure do |config|
   config.storage                             = :gcloud
-  config.gcloud_bucket                       = 'your-bucket-name'
-  #ToDo: Add bucket name
+  config.gcloud_bucket                       = 'works-with-image-upload'
   config.gcloud_bucket_is_public             = true
   config.gcloud_authenticated_url_expiration = 600
   config.gcloud_content_disposition          = 'attachment'
-
   config.gcloud_attributes = {
     expires: 600
   }
-
   config.gcloud_credentials = {
-    gcloud_project: 'gcp-project-name',
-    gcloud_keyfile: 'path-to-gcp-keyfile.json'
+    gcloud_project: 'works-with-222514',
+    gcloud_keyfile: '../works-with-0fe8b833e4b5.json'
   }
-
-  #ToDo: Add project data
 end
