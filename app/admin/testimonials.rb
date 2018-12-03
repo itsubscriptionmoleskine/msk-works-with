2e2a45f0b7059497f1885a02f24b7860b4a0181e
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-12-03 14:16:15 -0500

    basic databinding/styling for testimonial page

--- app/admin/testimonials.rb
+++ app/admin/testimonials.rb
@@ -7,6 +7,7 @@ ActiveAdmin.register Testimonial do
       f.input :title
       f.input :description, :input_html => {:class => 'tinymce'}, :label => 'Testimonial Copy'
       f.input :works_with_id, :label => 'Which "Works With" does this support?', :as => :select, :collection => WorksWith.all.map{|w| ["#{w.title}", w.id]}
+      f.input :video_embed, :label => 'Video Embed'
       f.input :is_live, :label => 'Publish?', :as => :radio
     end
     f.actions
--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -1,1 +1,13 @@
 #= require active_admin/base
+#= require tinymce
+
+$(document).ready ->
+  tinyMCE.init
+    mode: "textareas"
+    theme: "modern"
+    menubar:false
+    browser_spellcheck: true
+    plugins : 'link'
+  return
+
+
