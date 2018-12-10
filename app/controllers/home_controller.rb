082d6235090810067ab9e8b7c29c1e1c52009ecd
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-12-10 15:44:22 -0500

    closes #19

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     add_breadcrumb 'Home', home_path
     @testimonial_promos = Testimonial.all.last(3)
+    @featured_example = WorksWith.all.last(1)
   end
 end
