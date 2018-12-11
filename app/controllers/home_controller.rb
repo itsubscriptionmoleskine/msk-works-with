94a272f96824593c3f31deb6e83d2496de9ada9d
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-12-11 07:15:51 -0500

    sdoc generation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    ##
+    # Creates arrays of Testimonials and Works With
+    # for use with promotional components on the homepage
     add_breadcrumb 'Home', home_path
     @testimonial_promos = Testimonial.all.last(3)
     @featured_example = WorksWith.all.last(1)
