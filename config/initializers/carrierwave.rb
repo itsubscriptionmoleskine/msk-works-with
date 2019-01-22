e70b6092ac894caa969065182e590f6eb7c8c02f
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-01-22 09:03:16 +0100

    updates to initializers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/google'
   config.fog_credentials = {
       provider:                         'Google',
-      google_storage_access_key_id:     'GOOGZPQTHMUX23LNHTHTUUBZ',
-      google_storage_secret_access_key: 'BZWAPgIMqoTryTao682ESIhRyq4JTUOfFUasmYRH'
+      google_storage_access_key_id:     ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
+      google_storage_secret_access_key: ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
   }
   config.fog_directory = 'upload'
 end
