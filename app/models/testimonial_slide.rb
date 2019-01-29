4b2c16f96b0e32c5fc7cf1bddbca348624552b32
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-01-29 13:50:58 -0500

    removed carrierwave

--- app/models/testimonial_slide.rb
+++ app/models/testimonial_slide.rb
@@ -1,9 +1,7 @@
 class TestimonialSlide < ApplicationRecord
   belongs_to :testimonial
   accepts_nested_attributes_for :testimonial
-  mount_uploader :image_url, ImageUploader
   def as_json(options={})
     super(:only => [:image_url, :caption, :testimonial_id, :sequence_number, :is_live])
   end
-  validates :image_url, image_size: { width: { min: 1024 }, height: { min:768 } }
 end
