569e4d658aa613393fb489d76cb789886b8d39db
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-03-13 13:04:36 -0400

    attempt 5

--- db/migrate/20190307135549_video.rb
+++ db/migrate/20190307135549_video.rb
@@ -1,15 +1,18 @@
 class Video < ActiveRecord::Migration[5.2]
 
   def self.up
-    if ActiveRecord::Base.connection.table_exists? 'kittens' do
+
+    if ActiveRecord::Base.connection.table_exists? 'videos'
       drop_table :videos
     end
+
     create_table :videos do |t|
       t.string :youtube_url
       t.text :description
       t.boolean :is_live
       t.references :works_with, index: true
     end
+
   end
 
   def self.down
