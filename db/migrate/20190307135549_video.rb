7b9bc10dba57b41b70547629d3822cd97b8afa22
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-03-14 08:43:07 -0400

    attempt 12

--- db/migrate/20190307135549_video.rb
+++ db/migrate/20190307135549_video.rb
@@ -1,14 +1,14 @@
-class Video < ActiveRecord::Migration[5.2]
-  def self.up
-    create_table :videos do |v|
-      v.string :youtube_url
-      v.text :description
-      v.boolean :is_live
-      v.references :works_with, index: true
-    end
-  end
-
-  def self.down
-    drop_table :videos
-  end
-end
+# class Video < ActiveRecord::Migration[5.2]
+#   def self.up
+#     create_table :videos do |v|
+#       v.string :youtube_url
+#       v.text :description
+#       v.boolean :is_live
+#       v.references :works_with, index: true
+#     end
+#   end
+#
+#   def self.down
+#     drop_table :videos
+#   end
+# end
